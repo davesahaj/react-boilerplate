c64844c6d1995677beca74a8910ca7dd3f989a9a
Dave <sahajdavework@pm.me>
2024-01-09 01:25:08 +0530

    added tailwind, prettier, eslint, autoprefixer and postcss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import eslint from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    eslint({ cache: false, exclude: ['node_modules/**', 'vendor/**', 'app/**'] }),
+    react()
+  ],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, 'src/')
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss({}), autoprefixer({})]
+    }
+  }
+});
