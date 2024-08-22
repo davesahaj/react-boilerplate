0ba1059fac27d561e66330d2cf7253b20fe82ca0
Dave <sahajdavework@pm.me>
2024-08-22 12:36:33 +0530

    refactor: routes

--- src/providers/router.tsx
+++ src/providers/router.tsx
@@ -1,9 +1,11 @@
 import { publicRoutes } from '@/config'
-import { Route, Switch } from '@/libs'
+import { ROUTES } from '@/constants'
+import { Redirect, Route, Switch } from '@/libs'
 
 const RouterProvider = () => {
   return (
     <Switch>
+      <Route component={() => <Redirect to={ROUTES.HOME} replace />} path={ROUTES.BASE} />
       {publicRoutes.map(({ component: Component, path }) => (
         <Route component={Component} key={path} path={path} />
       ))}
